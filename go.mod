1d5dca2d32bf49ca13263b65896d386862c8b442
RideWindX <ridewindx@163.com>
2019-06-18 21:09:46 +0800

    Add go modules support and add gorm dialects import

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/cochainio/orm
+
+go 1.12
+
+require (
+	github.com/jinzhu/gorm v1.9.9
+	github.com/rs/xid v1.2.1
+)
--- orm.go
+++ orm.go
@@ -6,6 +6,10 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/rs/xid"
 )
 
